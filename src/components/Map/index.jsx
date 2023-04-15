fb71ca557b58f8512359c7d6bd6f11c7058cb4d8
acengzz <ahmet_cengiz_@outlook.com>
2023-04-15 23:44:13 +0300

    add user location

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,18 +1,26 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Popup, TileLayer } from "react-leaflet";
 
 import { TURKEY_CENTER } from "../../constants";
 import useEarthquakeData from "../../hooks/useEarthquakeData";
 
 import MarkerList from "../MarketList";
+import MarkerIcon from "../Marker";
 
 const Map = () => {
   const mapRef = useRef();
-
   const response = useEarthquakeData();
 
+  const [userGeoLocation, setUserGeoLocation] = useState(null);
+
   const mobileZoom = window.screen.width >= 280 && window.screen.width <= 780;
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition((position) =>
+      setUserGeoLocation(position)
+    );
+  }, []);
 
   return (
     <div className="map__container">
@@ -22,8 +30,23 @@ const Map = () => {
         className="map__map_container"
         center={TURKEY_CENTER}
       >
-        <TileLayer url={"https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=529BlBJJfeFAraNbX1NV"} />
+        <TileLayer
+          url={
+            "https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=529BlBJJfeFAraNbX1NV"
+          }
+        />
         <MarkerList earthquakeList={response.data} />
+        {userGeoLocation && (
+          <MarkerIcon
+            isUserIcon
+            position={{
+              lat: userGeoLocation.coords.latitude,
+              lng: userGeoLocation.coords.longitude,
+            }}
+          >
+            <Popup>You're here!</Popup>
+          </MarkerIcon>
+        )}
       </MapContainer>
     </div>
   );
--- src/components/Marker/index.jsx
+++ src/components/Marker/index.jsx
@@ -4,15 +4,21 @@ import { Icon } from "leaflet";
 import { Marker } from "react-leaflet";
 
 import MapPin from "../../assets/pin.png";
+import MapGreenPin from "../../assets/green_pin.png";
 
-const MarkerIcon = ({ children, position }) => {
+const MarkerIcon = ({ children, position, isUserIcon = false }) => {
   const markerIcon = new Icon({
     iconUrl: MapPin,
-    iconSize: [30, 30],
+    iconSize: [25, 25],
+  });
+
+  const userIcon = new Icon({
+    iconUrl: MapGreenPin,
+    iconSize: [22, 30],
   });
 
   return (
-    <Marker icon={markerIcon} position={position}>
+    <Marker icon={isUserIcon ? userIcon : markerIcon} position={position}>
       {children}
     </Marker>
   );
